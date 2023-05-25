f5c8e86f0331fb105f692361f44b99c8bd45ab4d
Maurizio Gallotti <117756194+Th4Dude@users.noreply.github.com>
2023-05-25 16:38:55 +0300

    Initial commit

---
+++ resources/views/admin/projects/edit.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <div class="d-flex align-items-center">
+        <h2 class="fs-4 text-secondary my-4">Modifica Progetto</h2>
+    </div>
+
+    <form action="{{ route('admin.projects.update', $project->id) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div class="mb-3">
+            <label for="title" class="form-label">Project Name</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $project->title) }}">
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <input type="text" class="form-control" id="description" name="description" value="{{ old('description', $project->description) }}">
+        </div>
+        <div class="mb-3">
+            <label class="form-label" for="start_date">Start date</label>
+            <input type="date" id="start_date" name="start_date" value="{{ old('start_date', $project->start_date) }}">
+
+            <label class="form-label ms-3" for="end_date">End date</label>
+            <input type="date" id="end_date" name="end_date" value="{{ old('end_date', $project->end_date) }}">
+        </div>
+
+
+    <div class="mb-3">
+        <label for="type_id" class="form-label">type</label>
+        <select class="form-select" name="type_id" id="type_id">
+        <option value="">Select type</option>
+        @foreach ($types as $type)
+            <option value="{{ $type->id }}" {{ old('type_id', $project->type_id) == $type->id ? 'selected' : '' }}>{{ $type->name }}</option>
+        @endforeach
+    </div>
+
+        <div class="mb-3">
+            <label for="image" class="form-label">Add Image</label>
+            <input class="form-control" type="file" id="formFile" name="image">
+        </div> 
+        
+        @if ($errors->any())
+
+            <div class="mb-3">
+                <div>Technology</div>
+                @foreach ($technologies as $technology)
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" name="technologies[]" id="technologies" value="{{ $technology->id }}" {{ in_array($technology->id, old('technologies', [])) ? 'checked' : '' }}>
+                    <label class="form-check-label" for="technologies">{{ $technology->name }}</label>
+                </div>
+                @endforeach
+            </div>
+
+        @else
+        
+            <div class="mb-3">
+                <div>Technology</div>
+                @foreach ($technologies as $technology)
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="checkbox" name="technologies[]" id="technologies" value="{{ $technology->id }}" {{ $project->technologies->contains($technology->id) ? 'checked' : '' }}>
+                        <label class="form-check-label" for="technologies">{{ $technology->name }}</label>
+                    </div>
+                @endforeach
+            </div>
+
+        @endif
+
+
+
+
+        <button type="submit" class="btn btn-primary btn-sm">Save Changes</button>
+    </form>
+</div>
+@endsection 
---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container col-6">
+    @if (session('message'))
+        <h5 class="alert alert-danger ms-auto p-2" role="alert">{{ session('message') }}</h5>
+    @endif
+    <h3 class="fs-4 text-secondary my-4 text-center"><strong>Project Name:</strong> {{ $project->title }}</h3>
+    <div class="d-flex justify-content-center">
+        <img src="{{asset('storage/' . $project->image)}}" alt="{{ $project->title }}" style="width: 450px; height: 450px; border-radius: 15px;">
+    </div>
+    <div class="d-flex justify-content-center">
+        <div class="py-4">
+            <p><strong>Description:</strong> {{ $project->description }}</p>
+            <p><strong>Type:</strong> {{ $project->type?->name ?: 'No selection' }}</p>
+            <p><strong>Technology:</strong>
+            @foreach ($project->technologies as $technology)
+            {{ $technology->name }}
+            @endforeach
+            </p>
+            <p><strong>Start date:</strong> {{ $project->start_date }}</p>
+            <p><strong>End date:</strong> {{ $project->end_date }}</p>
+            <p><strong>Slug:</strong> {{ $project->slug }}</p>
+            <a href="{{Route('admin.projects.index') }}" class="btn btn-sm btn-success">Go back</a>
+            <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-primary btn-sm">Make a change</a>
+            <a href="#" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#project-{{ $project->id }}">Delete</a>
+
+        </div>
+        
+
+
+    </div>
+    
+</div>
+
+<div class="modal fade" id="project-{{ $project->id }}" tabindex="-1" aria-hidden="true">
+
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                Are you sure you want to delete project Id <strong>{{ $project->id }}</strong>?
+            </div>
+            <div class="modal-footer">
+                <form action="{{ route('admin.projects.destroy', $project) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@endsection
