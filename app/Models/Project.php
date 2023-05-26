74f9503c14de5b615484941419ce131c87f7f3a5
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-26 13:12:11 +0300

    function image

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,9 +15,19 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
-
     public function technologies(){
         return $this->belongsToMany(Technology::class)->withTimestamps();
     }
-
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => strtoupper($value),
+        );
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string|null $value) => $value !== null ? asset('storage/' . $value) : null,
+        );
+    }
 }
