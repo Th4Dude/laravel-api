1764eadd09356b910a6a725f1f36a07091a98dc8
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-29 18:30:34 +0300

    api Type + Techonology controller + routes

---
+++ app/Http/Controllers/Api/TechnologyContoller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Technology;
+use Illuminate\Http\Request;
+
+class TechnologyContoller extends Controller
+{
+    public function index() {
+
+        $techologies = Technology::all();
+
+        return response()->json([
+            'success' => true,
+            'result' => $techologies
+        ]);
+    }
+    public function show(string $slug){
+        
+        $technology = Technology::where('slug', $slug)->with('projects')->first();
+
+        return response()->json([
+            'success' => true,
+            'results' => $technology
+        ]);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TypeController;
+use App\Http\Controllers\Api\TechnologyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +24,9 @@ use Illuminate\Support\Facades\Route;
 
  Route::get('projects',[ProjectController::class, 'index']);
  Route::get('projects/{slug}', [ProjectController::class, 'show']);
+ 
+ Route::get('types', [TypeController::class, 'index']);
+ Route::get('types/{slug}', [TypeController::class, 'show']);
+
+ Route::get('technologies', [TechnologyController::class, 'index']);
+ Route::get('technologies/{slug}', [TechnologyController::class, 'show']);
