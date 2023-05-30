0fe8c78fde2d651cd39e5d8179b9b2308c16c632
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-30 11:49:04 +0300

    fix!!

--- app/Http/Controllers/Api/TechnologyController.php
+++ app/Http/Controllers/Api/TechnologyController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Technology;
 use Illuminate\Http\Request;
 
-class TechnologyContoller extends Controller
+class TechnologyController extends Controller
 {
     public function index() {
 
